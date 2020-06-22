7c9cf8b7b764b68c315744cccd77fcd657a19e75
fenollhUbuntu <fenollh@gmail.com>
2020-06-22 11:19:19 +0200

    adicion de drawer navigation

--- ShoppingApp/src/components/header.js
+++ ShoppingApp/src/components/header.js
@@ -1,13 +1,27 @@
 import React from 'react'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 import {
     View,
+    Text,
     StyleSheet
 } from 'react-native'
+
+const UserIcon = <Ionicons name='md-person' size={60} style={{alignSelf: 'center'}}/>
+const SettingsIcon = <Ionicons name='ios-settings' size={60} style={{alignSelf: 'center'}}/>
+
 
 const Header = _ => {
     return(
         <View style={styles.container}>
-
+            <View style={{flex:1, justifyContent: 'center'}}>
+                {SettingsIcon}
+            </View>
+            <View style={{flex:2, justifyContent: 'center'}}>
+                <Text style={styles.title}>SHOPPING APP</Text>
+            </View>
+            <View style={{flex:1, justifyContent: 'center'}}>
+                {UserIcon}
+            </View>
         </View>
     );
 }; export default Header
@@ -15,6 +29,12 @@ const Header = _ => {
 const styles = StyleSheet.create({
     container:{
         flex: 0.1, 
-        backgroundColor: 'green',
+        flexDirection: 'row',
+        backgroundColor: 'rgb(255,255,255)',
     },
+    title:{
+        alignSelf: 'center',
+        fontSize: 18,
+        fontWeight: 'bold',
+    }
 })
--- ShoppingApp/src/components/router.js
+++ ShoppingApp/src/components/router.js
@@ -1,26 +1,87 @@
 import React from 'react'
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import Ionicons from 'react-native-vector-icons/Entypo'
+
 import { 
-    View 
+    View,
+    StyleSheet,
 } from 'react-native';
+
+
 import ShoppingList from '../screens/shoppingList'
 import Shops from '../screens/shops'
 
+
 const Tab = createMaterialTopTabNavigator();
+const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default class Router extends React.Component{
+    
+    
+    TabNavigator({navigation}) {
+        return(
+            <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({}) => {
+                    let iconName;
+                    if (route.name === 'ShoppingList')  { iconName = 'list' }
+                    else if (route.name === 'Shops') { iconName = 'shop' }
+                    return <Ionicons name={iconName} size={25} color='rgb(52,251,167)' />;
+                },
+            })}      
+            tabBarOptions={{
+                showIcon: true,
+                showLabel: false,
+            }}>
+                <Tab.Screen name="ShoppingList" component={ShoppingList} />
+                <Tab.Screen name="Shops" component={Shops} />
+            </Tab.Navigator>
+        )
+    }
+
+    StackNavigator({navigation}) {
+        return(
+            <Stack.Navigator headerMode= 'none'>
+                <Stack.Screen name="main" component={this.TabNavigator} />
+            </Stack.Navigator>
+        )
+    }
+
     render(){
         return(
             <View style={{flex:1}}>
-                <NavigationContainer>
-                    <Tab.Navigator>
-                        <Tab.Screen name="ShoppingList" component={ShoppingList} />
-                        <Tab.Screen name="Shops" component={Shops} />
-                    </Tab.Navigator>
+                <NavigationContainer theme={MyTheme}>
+                    <Drawer.Navigator 
+                        drawerStyle={styles.drawer}
+                        drawerContentOptions={{
+                            activeTintColor: '#e91e63',
+                            itemStyle: { marginVertical: 30 },
+                        }}>
+                        <Drawer.Screen name="main" component={this.TabNavigator} options={{
+                            drawerIcon: config => <Ionicons name='list' size={25} color='rgb(52,251,167)' />
+                        }}/>
+                    </Drawer.Navigator>
                 </NavigationContainer>
             </View>
         )
     }
 }
+const MyTheme = {
+    colors: {
+        primary: 'rgb(52,251,167)', 
+        card: 'rgb(80,130,255)',
+    },
+};
+
+const styles = StyleSheet.create({
+    drawer:{
+        backgroundColor: 'rgb(100,100,100)',
+        width: 250,
+    },
+})
