e69fb6e658c5939313192f1a8d9d22c8ce4f8146
fenollhUbuntu <fenollh@gmail.com>
2020-06-23 10:36:56 +0200

    esquema login

--- ShoppingApp/src/screens/login.js
+++ ShoppingApp/src/screens/login.js
@@ -11,9 +11,18 @@ export default class LoginScreen extends React.Component{
     render(){
         return(
             <View style={styles.container}>
-                <TouchableOpacity style={{height: 100, width: 100, backgroundColor: 'blue'}} onPress={()=> this.props.navigation.navigate('Main')}>
+                <View style={styles.header}><Text style={styles.title}> LOGIN </Text></View>
+                <View style={{flex: 1}}/>
+                <View style={{flex: 4, padding: '2%'}}>
+                    <View style={styles.formBox}>
 
-                </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={{flex:2, backgroundColor: 'red'}}>
+                    <TouchableOpacity></TouchableOpacity>
+                </View>
+
+                {/*<TouchableOpacity style={{height: 100, width: 100, backgroundColor: 'blue'}} onPress={()=> this.props.navigation.navigate('Main')}/>*/}
             </View>
         )
     }
@@ -21,6 +30,25 @@ export default class LoginScreen extends React.Component{
 const styles = StyleSheet.create({
     container:{
         flex:1,
-        backgroundColor: 'green',
+        padding: '3%',
+        backgroundColor: 'rgb(220,245,240)'
     },
+    header:{
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    title: {
+        alignSelf: 'center',
+        fontWeight: 'bold',
+        color: 'rgb(52,167,251)',
+        fontSize: 30
+    },
+    formBox: {
+        flex: 1,
+        borderRadius: 20,
+        borderColor: 'rgb(52,167,251)',
+        borderWidth: 1,
+        backgroundColor: 'rgb(220,230,255)'
+    }
 })
