78cae0acc08b0e77be2802c3d18c72ffc189daf7
fenollhUbuntu <fenollh@gmail.com>
2020-08-30 22:17:07 +0200

    userDataEditor structured

--- Server/userDataEditor/main.go
+++ Server/userDataEditor/main.go
@@ -1,11 +1,5 @@
 package main
 
-func main() {
-
-}
-
-/*
-
 import (
 	"database/sql"
 	"encoding/json"
@@ -20,8 +14,25 @@ import (
 var db *sql.DB
 var err error
 
-// GetPublicUser selects all  the ppublic data about a user without authentication
-func GetPublicUser(w http.ResponseWriter, req *http.Request) {
+// EditUser chages a parameter in user data
+func EditUser(w http.ResponseWriter, req *http.Request) {
+	var data UserDataEdition
+	json.NewDecoder(req.Body).Decode(&data)
+	// The payload is receibed as a string so we need to cast this value to the supported to the DB
+	switch data.COLUMN {
+	case "age":
+		//cast to int
+		break
+	case "stars":
+		//cast to int
+		break
+	default:
+		//dont modify the data
+		break
+	}
+	// update the data in the DB
+	// handle all posible errors
+	json.NewEncoder(w).Encode(data)
 }
 
 func main() {
@@ -34,9 +45,8 @@ func main() {
 	router := mux.NewRouter()
 
 	//endpoints
-	router.HandleFunc("/", GetPublicUser).Methods("POST")
+	router.HandleFunc("/", EditUser).Methods("PUT")
 
 	log.Fatal(http.ListenAndServe(":3000", router))
 	defer db.Close()
 }
-*/
--- Server/userDataEditor/types.go
+++ Server/userDataEditor/types.go
@@ -1,1 +1,7 @@
 package main
+
+// UserDataEdition where the data about the edition is stores
+type UserDataEdition struct {
+	COLUMN  string `json:"column"`
+	PAYLOAD string `json:"payload"`
+}
